7096c4db8d546aff200e134e0a1e05b9fd448d5d
kee nam <nam.keeyeon@gmail.com>
2017-10-16 12:27:58 -0700

    Added self method to users_controller.rb, logout route to routes.rb; working on users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: [:login]
+
   def index
     @users = User.all
   end
@@ -6,5 +8,23 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
+  end
+
+  def login
+    auth_hash = request.env['omniauth.auth']
+    if auth_hash['uid']
+      user = User.find_by(provider: params[:provider], uid: auth_hash['uid'])
+      if user.nil?
+      else
+      end # end user.nil? if-else 
+    else
+    end # end auth_hash['uid'] if-else
+  end
+
+  def logout
+    session[:user_id] = nil
+    flash[:status] = :success
+    flash[:message] = "Successfully logged out!"
+    redirect_to root_path
   end
 end
